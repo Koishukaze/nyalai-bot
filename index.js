55a7c33ae7b8355670e2fc1f2f7a170902738d80
Koishukaze <47912135+Koishukaze@users.noreply.github.com>
2022-01-16 16:51:13 +1100

    NyalAi Alpha
    Added command folder and comments

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
-const Discord = require("discord.js")
+const Discord = require('discord.js');
 require("dotenv").config()
 
 const client = new Discord.Client({
-    intents: [,
+    intents: [
         "GUILDS",
         "GUILD_MESSAGES"
     ]
 })
 
-client.on ("ready", () => {
+//Sends startup message in console
+client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`)
 })
 
-client.on("messageCreate", (message) => {
-    if (message.content == "hi"){
-        message.reply("Hello World!")
-    }
-})
+//Sets bot prefix to designated character
+const prefix = ';';
+const fs = require('fs');
 
-client.login(process.env.TOKEN)
+client.commands = new Discord.Collection();
+
+//Reads commands in commands folder
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
+//Executes indiviudal command in commands folder
+client.on('messageCreate', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'ping') {
+        client.commands.get('ping').execute(message, args);
+    }
+});
+
+//Login token
+client.login(process.env.TOKEN);
